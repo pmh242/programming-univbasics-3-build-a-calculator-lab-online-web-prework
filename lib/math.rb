d0a8a6c5a1897dc80785029e004e2b17eafbfc29
Pedro Huertas <pmh242@gmail.com>
2019-10-20 08:35:33 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num2 + num1
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  minus = num2 - num1
+  puts = minus
 end
 
 def division(num1, num2)
-
+  divide = num2 // num1
+  puts divide
 end
 
 def multiplication(num1, num2)
-
+  multiple = num1 * num2
+  puts multiple
 end
 
 def modulo(num1, num2)
-
+  mod = num2 % num1
+  puts mod
 end
 
 def square_root(num)
-
+  sqft = num
+  puts sqft
 end
